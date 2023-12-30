b5668fe77a2b0379c64ed4afe2b39e4df505de86
polina <palinagolgaya@yandex.ru>
2023-12-30 15:21:54 +0300

    +- начал двигаться только один пылесос

--- main.py
+++ main.py
@@ -4,46 +4,21 @@ from pygame.locals import *
 import random
 
 
-class AnimatedSprite(pygame.sprite.Sprite):
-    def __init__(self, sheet, sheet2, columns, rows, x, y):
-        super().__init__(all_sprites)
-        self.frames = []
-        self.cut_sheet(sheet, columns, rows)
-        self.cut_sheet2(sheet2, columns, rows)
-        self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect.topleft = (x, y)
-        self.start_time = pygame.time.get_ticks()
-        self.delay = 50
-
-    def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
-
-    def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
-        for j in range(rows):
-            for i in range(columns):
-                frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
-
-    def update(self):
-        self.cur_frame = ((pygame.time.get_ticks() - self.start_time) // self.delay) % len(self.frames)
-        self.image = self.frames[int(self.cur_frame)]
-
-
 class Cleaner(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__(all_sprites)
-        self.cleaner = pygame.image.load('cleaner.jpeg')
-        self.cleaner = pygame.transform.scale(self.cleaner, (100, 100))
+        self.image = pygame.image.load('cleaner.jpeg')
+        self.image = pygame.transform.scale(self.image, (130, 130))
+        self.image.set_colorkey('white')
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = y
 
+    def move(self):
+        self.rect.x += 1
 
 class Puddle(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__(all_sprites)
+    pass
 
 
 class Slipers(pygame.sprite.Sprite):
@@ -71,18 +46,17 @@ class Cockroach(pygame.sprite.Sprite):
 
 
 def start_fon():
-    fon = pygame.image.load('floor.jpg')
-    fon = pygame.transform.scale(fon, (725, 725))
-    screen.blit(fon, (0, 0))
     for i in range(3):
         num = random.choice([1, 2, 3])
         # 1 cleaner
         # 2 puddle
         # 3 slipers
+        print(num)
         if num == 3:
             Slipers(0, 145 * i)
-        # elif num == 1:
-        #     Cleaner(0, 145 * i)
+        elif num == 1:
+            cleaners.append(Cleaner(0, 145 * i))
+
         # else:
         #     Puddle(0, 145 * i)
 
@@ -93,23 +67,26 @@ fpsClock = pygame.time.Clock()
 width, height = 725, 725
 screen = pygame.display.set_mode((width, height))
 all_sprites = pygame.sprite.Group()
+cleaners = []
+fon = pygame.image.load('floor.jpg')
+fon = pygame.transform.scale(fon, (725, 725))
+screen.blit(fon, (0, 0))
 start_fon()
 Cockroach(all_sprites)
 
-# sheet = pygame.image.load('bear.jpg').convert_alpha()
-# AnimatedSprite(sheet, 6, 2, 300, 435)
-# sheet2 = pygame.image.load('slippers.jpg').convert_alpha()
-# AnimatedSprite(sheet2, 7, 1, 400, 500)
-
 while True:
+    screen.blit(fon, (0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
     # Update
+    for elem in cleaners:
+        elem.move()
 
     # Draw
     all_sprites.draw(screen)
+
     pygame.display.flip()
     fpsClock.tick(fps)
